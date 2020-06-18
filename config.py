99bfe997edc39cc2fa05e236be12da1fa2177460
Ken Tsang <tsangkakuen@shkp.com>
2020-06-18 14:21:06 +0800

    init

---
+++ config.py
@@ -0,0 +1,8 @@
+import os, json
+
+with open('./config.json') as config_file:
+    config = json.load(config_file)
+
+class Config:
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
